480d33201c0130cfe11f0b3575329d693f084230
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-12-28 19:32:02 -0800

    adding FE reqs - mutations.js

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,17 @@
+// Front-End Specifications
+// You'll need to create the following front-end files:
+
+import { gql } from '@apollo/client';
+
+//Expects 2 variables. Returns token and user
+export const LOGIN_USER = gql``;
+
+// mutations.js:
+
+// LOGIN_USER will execute the loginUser mutation set up using Apollo Server.
+
+// ADD_USER will execute the addUser mutation.
+
+// SAVE_BOOK will execute the saveBook mutation.
+
+// REMOVE_BOOK will execute the removeBook mutation.
