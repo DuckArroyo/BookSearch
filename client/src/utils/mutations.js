683a63ea9d526539dd1fe8513986d2e4bc0f1248
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-12-30 21:46:29 -0800

    adding FE  mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -19,7 +19,37 @@ export const LOGIN_USER = gql`
 `;
 
 // ADD_USER will execute the addUser mutation.
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
 
 // SAVE_BOOK will execute the saveBook mutation.
+export const SAVE_BOOK = gql`
+  mutation saveBook(title: String!) {
+    addTBook(title: $title) {
+    _id
+    title
+    authors
+    description
+    link
+    }
+  }
+`;
 
 // REMOVE_BOOK will execute the removeBook mutation.
+export const REMOVE_BOOK = gql`
+  mutation removeBook($id: ID!) {
+    removeBook(id: $id) {
+      _id
+      title
+    }
+  }
+`;
