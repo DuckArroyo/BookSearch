403719b5995caf4fb82574fc75574ce9c172efa0
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-12-30 21:32:50 -0800

    adding FE  mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -3,12 +3,20 @@
 
 import { gql } from '@apollo/client';
 
-//Expects 2 variables. Returns token and user
-export const LOGIN_USER = gql``;
-
 // mutations.js:
 
 // LOGIN_USER will execute the loginUser mutation set up using Apollo Server.
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
 
 // ADD_USER will execute the addUser mutation.
 
